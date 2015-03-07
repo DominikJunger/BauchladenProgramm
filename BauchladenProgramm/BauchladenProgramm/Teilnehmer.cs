a677d4ecb12e328f947664ebaa4f287206a38be4
Kiba <kiba35@web.de>
2015-03-07 22:53:47 +0100

    Revert "sdf"
    This reverts commit f953bddfaac0bbf8d4cf8df79ec19e31e3dc9d1d.

---
+++ BauchladenProgramm/BauchladenProgramm/Teilnehmer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BauchladenProgramm;
+
+namespace BauchladenProgramm
+{
+    public class Teilnehmer: Person
+    {
+        private int id;
+        private Konto konto;
+
+       
+
+        private static int naechsteId = 1;
+
+        public Teilnehmer(string vorname, string nachname, DateTime geburtsdatum, string wohnort):base (vorname, nachname,geburtsdatum, wohnort)
+        {
+            this.id = naechsteId;
+            naechsteId++;
+            konto = new Konto();           
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public Konto Konto
+        {
+            get { return konto; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj.GetType() == typeof (Teilnehmer)))
+                return false;
+            Teilnehmer t = (Teilnehmer)obj;
+            if (this.id == t.id)
+                return true;
+            else
+                return false;
+        }
+    }
+}
