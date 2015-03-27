2bcaef23420035973cd5ef0f5c9df8016e797a67
Kiba <kiba35@web.de>
2015-03-27 18:07:47 +0100

    Kleinere Arbeiten an SQL dingens

--- BauchladenProgramm/BauchladenProgrammServer/Klassen/SQL_Connector.cs
+++ BauchladenProgramm/BauchladenProgrammServer/Klassen/SQL_Connector.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Data;
 using System.IO;
+using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 
@@ -17,17 +19,35 @@ namespace BauchladenProgrammServer.Klassen
         private string userID;       
         private string password;       
         private string asynchProcessing;
-        private string[] results = new string[1000];
+
         private SqlConnection con;
+        private Form x;
+        Label l1;
 
         public SQL_Connector(string dataSource = @"Data Source=QUAK-ENTE\SQLEXPRESS;", string initialCatalog = "Initial Catalog=DBTest;", string persistSecurity = "Persist Security Info=True;", string userID = "User ID=DBLoginTest;", string password = "Password=12345;", string asynchProcessing = "Asynchronous Processing=True")
-        { 
+        {
+            this.x = new Form();
+            l1 = new Label();
+            x.Controls.Add(l1);
             this.DataSource = dataSource;
             this.InitialCatalog = initialCatalog;
             this.PersistSecurity = persistSecurity;
             this.UserID = userID;
             this.Password = password;
             this.AsynchProcessing = asynchProcessing;   
+        }
+
+        public ConnectionState openConnection()
+        {
+            con = new SqlConnection(DataSource + InitialCatalog + PersistSecurity + UserID + Password + AsynchProcessing);
+            con.Open();
+
+            return con.State;
+       }
+
+        public void closeConnection()
+        {
+            con.Close();
         }
 
         public string DataSource
--- BauchladenProgramm/BauchladenProgrammServer/ServerMain.cs
+++ BauchladenProgramm/BauchladenProgrammServer/ServerMain.cs
@@ -13,9 +13,39 @@ namespace BauchladenProgrammServer
 {
     public partial class Mainwindow : Form
     {
+        private SQL_Connector con;
+      
         public Mainwindow()
         {
             InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox4.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox4.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void Mainwindow_Load(object sender, EventArgs e)
+        {
+            con = new SQL_Connector();
+            this.textBox1.Text = con.DataSource.Substring(con.DataSource.IndexOf('=') + 1);
+            this.textBox2.Text = con.InitialCatalog.Substring(con.InitialCatalog.IndexOf('=') + 1);
+            this.textBox3.Text = con.UserID.Substring(con.UserID.IndexOf('=') + 1);
+            this.textBox4.Text = con.Password.Substring(con.Password.IndexOf('=') + 1);
+
+            con.openConnection();
+        }
+
+        private void Mainwindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            con.closeConnection();
         }
     }
 }
